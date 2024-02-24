3b4b0940060be1d0927ebc4927769f2a5cf8ef83
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-23 22:42:25 -0300

    v1.53
    - Reestruturando App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Router } from "./src/Router";
+import { Router } from "./src/Router/index";
 import { CepProvider } from "./src/Providers/CEP/";
 import { StatusBar } from "expo-status-bar";
+import { AuthProvider } from "./src/Providers/Auth";
 
 export default function App() {
   return (
     <>
       <StatusBar hidden />
       <NavigationContainer>
-        <CepProvider>
-          <Router />
-        </CepProvider>
+        <AuthProvider>
+          <CepProvider>
+            <Router />
+          </CepProvider>
+        </AuthProvider>
       </NavigationContainer>
     </>
   );

2a9a40efc49ad1a931686798f8a88f286854a171
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-24 16:46:31 -0300

    v1.75
    - Usando styled-components na tela User também

--- src/Pages/User/index.tsx
+++ src/Pages/User/index.tsx
@@ -1,92 +1,52 @@
 import { useContext } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TextInput,
-  SafeAreaView,
-  Platform,
-} from "react-native";
+import { Text, TouchableOpacity, Platform } from "react-native";
+import { Container, Logo, Input, ButtonLogin, ButtonLoginText } from "./styles";
 import { AuthContext } from "../../Providers/Auth";
 import { signOutFunction } from "../../Services/auth/signout";
 
 export default function User() {
+  const { user, setUser } = useContext(AuthContext);
 
-  const { user, setUser } = useContext(AuthContext)
-
-  async function handleSignOut(){
-    setUser(undefined)
-    signOutFunction()
+  async function handleSignOut() {
+    setUser(undefined);
+    signOutFunction();
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.logo}>Usuário</Text>
+    <Container>
+      <Logo style={{ marginTop: Platform.OS === "android" ? 55 : 80 }}>
+        ViaCep App
+      </Logo>
       <Text style={{ marginBottom: 20 }}>
-        Veja seus dados!
+        Cadastre seus endereços, agilize sua vida!
       </Text>
 
-      <TextInput
+      <Input
         editable={false}
-        style={styles.input}
+        value={user?.name}
+        placeholder="Qual seu nome?"
+        placeholderTextColor="#99999B"
+      />
+
+      <Input
+        editable={false}
         value={user?.email}
         placeholder="Seu email"
         placeholderTextColor="#99999B"
       />
 
-      <TextInput
+      <Input
         editable={false}
-        style={styles.input}
         value={user?.senha}
         placeholder="Sua senha"
         placeholderTextColor="#99999B"
       />
 
-      <TouchableOpacity
-        style={[
-          styles.buttonLogin,
-          { backgroundColor: "#F53745" },
-        ]}
+      <ButtonLogin
         onPress={handleSignOut}
       >
-        <Text style={styles.buttonText}>Sair</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+        <ButtonLoginText>Sair</ButtonLoginText>
+      </ButtonLogin>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "#FFF",
-  },
-  logo: {
-    marginTop: Platform.OS === "android" ? 55 : 80,
-    fontSize: 28,
-    fontWeight: "bold",
-  },
-  input: {
-    color: "#121212",
-    backgroundColor: "#EBEBEB",
-    width: "90%",
-    borderRadius: 6,
-    marginBottom: 10,
-    paddingHorizontal: 8,
-    height: 50,
-  },
-  buttonLogin: {
-    width: "90%",
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 10,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: "#FFF",
-    fontWeight: "bold",
-    fontSize: 19,
-  },
-});
